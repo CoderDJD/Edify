1ad111663001fdfc389f7d471b1b6c14f581a101
Joshua Daniel D <natechnologies2007@gmail.com>
2021-08-27 10:24:59 +0530

    final v5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import Navigator from "./util/Navigator";
 import React, { lazy, Suspense } from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 const Main = lazy(() => import("./packages/main"));
 const Create = lazy(() => import("./packages/create"));
 
@@ -8,13 +8,11 @@ export default function App() {
   return (
     <Suspense fallback={<div className="bg-dark-400 w-screen h-screen"></div>}>
       <div className="bg-dark-400 w-screen h-screen">
-        <BrowserRouter>
-          <Switch>
-            <Route component={Main} path="/" />
-            <Route component={Create} path="/create" />
-          </Switch>
-          <Navigator />
-        </BrowserRouter>
+        <Switch>
+          <Route component={Main} path="/" />
+          <Route component={Create} path="/create" />
+        </Switch>
+        <Navigator />
       </div>
     </Suspense>
   );
--- server/src/format.js
+++ server/src/format.js
@@ -1,6 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
-import monk from "monk";
+require("dotenv").config();
+const monk = require("monk");
 const db = monk(process.env.MONGO_URI ? process.env.MONGO_URI : "");
 const reqs = db.get("reqs");
 reqs.drop();
